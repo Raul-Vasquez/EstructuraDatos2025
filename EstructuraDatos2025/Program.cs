a218ab60860a2e00c4361a02026f5da068cf6a92
Raúl Vásquez <rs.vasquezl@uea.edu.ec>
2024-12-12 16:34:59 -0500

    Practica de la semana de las dos figuras

--- EstructuraDatos2025/Program.cs
+++ EstructuraDatos2025/Program.cs
@@ -1,3 +1,10 @@
 ﻿// Console.WriteLine se utiliza para imprimir en pantalla
-Console.WriteLine("Hola Mundo--->");
-Console.WriteLine(4+9);
+Console.WriteLine("UNIVERSIDAD ESTATAL AMAZONICA--->");
+Console.WriteLine("AUTOR: RAUL VASQUEZ LASCANO");
+
+Rectangulo miRectangulo = new Rectangulo (5.2 , 5.4);
+Console.WriteLine("El area del rectnagulo es: "+miRectangulo.Area());
+
+Pentagono miPentagono = new Pentagono (7.2,5.0);
+Console.WriteLine ("Area del pentagóno: "+ miPentagono.Area2()+ "cm");
+
