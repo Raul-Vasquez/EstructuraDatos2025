09f53634bc94da4c21e1435afaac47aa3a64a366
Raúl Vásquez <rs.vasquezl@uea.edu.ec>
2024-12-20 11:23:15 -0500

    Subia de la semana3

--- EstructuraDatos2025/obj/Debug/net9.0/EstructuraDatos2025.AssemblyInfo.cs
+++ EstructuraDatos2025/obj/Debug/net9.0/EstructuraDatos2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstructuraDatos2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f81dd9e51e499f9eacf941fd0e9aeb1154f6f1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f8765e08b3e1354fea6fcae216fef5f3895bc83")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstructuraDatos2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstructuraDatos2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
