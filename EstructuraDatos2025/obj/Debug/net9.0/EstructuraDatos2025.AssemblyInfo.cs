c9f401f4f852664b6220f4b5ca7a2299f9a52056
Raúl Vásquez <rs.vasquezl@uea.edu.ec>
2025-01-12 21:31:47 -0500

    Semana_5

--- EstructuraDatos2025/obj/Debug/net9.0/EstructuraDatos2025.AssemblyInfo.cs
+++ EstructuraDatos2025/obj/Debug/net9.0/EstructuraDatos2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstructuraDatos2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97f54c9821af30f15c5132ed744fb6d9ec50dd92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c27942a7c5551324fe6d1884bcf5e518399ff861")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstructuraDatos2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstructuraDatos2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
