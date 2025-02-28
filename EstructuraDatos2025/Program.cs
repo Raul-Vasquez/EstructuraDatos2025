c5e9974943a964f4aea9376e44c7f20b4d243aed
Raúl Vásquez <rs.vasquezl@uea.edu.ec>
2025-02-28 10:30:24 -0500

    Semana 11 Tarea sobre la traduccion de palabras

--- EstructuraDatos2025/Program.cs
+++ EstructuraDatos2025/Program.cs
@@ -1,2 +1,6 @@
-﻿﻿
-Operaciones_Conjutos.run();
+﻿﻿/*Encabezado*/
+Console.WriteLine("Universidad Estatal Amazonica");
+Console.WriteLine("Carrera de Tecnologias de la Información");
+Console.WriteLine("Modalidad Virtual\n");
+//Llamda a la Clase para que se ejecute el programa
+DiccionarioTraductor.run();
